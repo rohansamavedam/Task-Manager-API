2be6885685e0d6165822b1360d8073768df3b83f
Rohan Samavedam <rohansamavedam@gmail.com>
2019-07-04 19:20:09 +0530

    CRUD operations added + Promise update

--- mongodb.js
+++ mongodb.js
@@ -9,15 +9,43 @@ const { MongoClient, ObjectID } = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID
-console.log(id)
-console.log(id.getTimestamp())
+// const id = new ObjectID
+// console.log(id)
+// console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if(error){
        return console.log('unable to connect to database')
     }
     const db = client.db(databaseName)
+
+    // db.collection('users').findOne({ name: 'Jen' }, (error, user) => {
+    //     if(error){
+    //         return console.log('unable to fetch')
+    //     }
+    //     console.log(user)
+    // })
+
+    // db.collection('tasks').findOne({_id: new ObjectID('5d186e3c04e3a8cac9967b8f')}, (error, task) => {
+    //     if(error){
+    //         return console.log('cannot fetch')
+    //     }
+    //     console.log(task)
+    // })
+
+    db.collection('tasks').find({completed: true}).toArray((error, tasks)=>{
+        if(error){
+            return console.log('cannot fetch')
+        }
+        console.log(tasks)
+    })
+
+
+
+
+
+
+
     // db.collection('users').insertOne({
     //     name: 'Rohan',
     //     age: 20
