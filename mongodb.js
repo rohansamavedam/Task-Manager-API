f3277a763960a70eb3b2c4996fa2c78888ce84c1
Rohan Samavedam <rohansamavedam@gmail.com>
2019-07-04 19:21:41 +0530

    CRUD operations added + Promise update

--- mongodb.js
+++ mongodb.js
@@ -9,29 +9,15 @@ const { MongoClient, ObjectID } = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-// const id = new ObjectID
-// console.log(id)
-// console.log(id.getTimestamp())
+const id = new ObjectID
+console.log(id)
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if(error){
        return console.log('unable to connect to database')
     }
     const db = client.db(databaseName)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     db.collection('users').findOne({ name: 'Jen' }, (error, user) => {
         if(error){
